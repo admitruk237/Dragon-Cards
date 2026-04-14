a9b7ade327a1e269bae005b09a06a1a293e80235
Andrii Dmtryk <admitruk237@gmail.com>
2026-04-14 13:56:31 +0200

    feat: add dnd sorting for dragon cards

---
+++ src/widgets/game-field/ui/SortableCard.tsx
@@ -0,0 +1,31 @@
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { DragonCard } from '@/entities/card/ui/DragonCard';
+import { cn } from '@/lib/utils';
+
+interface Props {
+  id: string;
+  isRevealed?: boolean;
+  isWinner?: boolean;
+  multiplier?: number | string;
+  dragonType?: string;
+  type: 'top' | 'bottom';
+}
+
+export const SortableCard = (props: Props) => {
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+    id: props.id,
+  });
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={{ transform: CSS.Transform.toString(transform), transition }}
+      {...attributes}
+      {...listeners}
+      className={cn(isDragging ? 'opacity-30, z-100' : 'opacity-100 z-1')}
+    >
+      <DragonCard {...props} />
+    </div>
+  );
+};
